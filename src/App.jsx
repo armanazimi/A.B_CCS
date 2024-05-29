7ce28b20392b96f50c85fee3bfa75555c613396b
AlirezaBolhasani-Pr <167274715+AlirezaBolhasani-Pr@users.noreply.github.com>
2024-05-29 22:39:39 +0330

    comment
    App.jsx hellp

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
-import React from 'react'
+import React from "react";
 
 // scss
-import './App.css'
+import "./App.css";
 
 // Component
-import Auction from './Components/Auction/Auction'
-import Navbar from './Components/Navbar/Navbar'
-import Home from './Components/Home/Home'
-import Search from './Components/Search/Search'
-import Trending from './Components/Trending/Trending'
-import Sellers from './Components/Sellers/Sellers'
-import Review from './Components/Review/Review'
-import Footer from './Components/Footer/Footer'
+import Auction from "./Components/Auction/Auction";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
+import Search from "./Components/Search/Search";
+import Trending from "./Components/Trending/Trending";
+import Sellers from "./Components/Sellers/Sellers";
+import Review from "./Components/Review/Review";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className='title'>
-      <Navbar/>
-      <Home/>
-      <Search/>
-      <Trending/>
-      <Sellers/>
-      <Auction/>
-      <Review/>
-      <Footer/>
+    <div className="title">
+      {/* navbar */}
+      <Navbar />
+      {/* home */}
+      <Home />
+      {/* search */}
+      <Search />
+      {/* trending */}
+      <Trending />
+      {/* swllers */}
+      <Sellers />
+      {/* auction */}
+      <Auction />
+      {/* reviwe */}
+      <Review />
+      {/* footer */}
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
